157ff5896a8a030a14d630388365886b4f32af77
sh5080 <prpn97@gmail.com>
2025-05-06 00:33:29 +0900

    feat: 상수 정의

---
+++ pkg/types/constants.go
@@ -0,0 +1,34 @@
+package constants
+
+// 스티커 도메인 패턴
+var STICKER_DOMAINS = []string{
+	"storep-phinf.pstatic.net",
+	"post-phinf.pstatic.net",
+	"cometoplay.kr",
+	"reviewnote.co.kr",
+}
+
+// 스티커 클래스 패턴
+var STICKER_CLASSES = []string{
+	"se-sticker",
+	"sticker",
+	"_img",
+	"sponsor-tag",
+	"ad-tag",
+	"se-module",
+	"se-module-image",
+	"se-image-resource",
+}
+
+// 콘텐츠 영역 선택자
+var CONTENT_SELECTORS = []string{
+	".se-main-container", // 스마트에디터 2.0
+	".post_ct",           // 구버전 모바일
+	"#viewTypeSelector",  // 구버전 PC
+	".se_component_wrap", // 구버전 PC (스마트에디터 1.0)
+	".se-module-text",    // 텍스트 모듈
+	".sect_dsc",          // 모바일 본문
+	".se_card_container", // 카드 컨테이너
+	"#postViewArea",      // 일반 포스트
+	".post-content",      // 일반적인 블로그 본문 클래스
+}
